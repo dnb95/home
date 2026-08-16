b3ebbd103bdd25761a2d811a8e13edd59a3fb431
DnB <dan.bouillet.14@gmail.com>
2026-08-16 11:13:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -906,7 +906,7 @@ window.togglePushNotifs = async (enabled) => {
       return;
     }
 
-    const registration = await navigator.serviceWorker.register("/firebase-messaging-sw.js");
+    const registration = await navigator.serviceWorker.register("sw.js");
     const messaging = getMessaging(app);
     const token = await getToken(messaging, {
       vapidKey: VAPID_PUBLIC_KEY,
